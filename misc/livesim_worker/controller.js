88ab9171dc6b56c17cddd0b57d4f16827d320322
Sina <sina@mascd10.SOE.UCSC.EDU>
2015-12-08 03:31:05 -0800

    missing license

--- misc/livesim_worker/controller.js
+++ misc/livesim_worker/controller.js
@@ -1,3 +1,20 @@
+/*
+ * (C) Copyright 2015 Regents of the University of California and LiveSim Project.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-2.1.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * Contributors:
+ *     Sina Hassani
+ */
+
 var socket_io_client = require('socket.io-client');
 //var httpsc = require('https');
 var exec = require('child_process').exec;
